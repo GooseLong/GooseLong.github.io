89029435adc1e084bb4e64f27347946e5d09ead2
Angus Long <0362654@schoolsnet.act.edu.au>
2020-07-11 11:21:42 +1000

    implement boilerplate

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,81 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser
+import Browser.Events
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Html
+
+
+
+-- MAIN
 
 
 main =
-    text "hello world"
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : { width : Int, height : Int } -> ( Model, Cmd msg )
+init windowSize =
+    ( { character = CharacterA
+      , orientation = .orientation <| classifyDevice windowSize
+      }
+    , Cmd.none
+    )
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { character : Character
+    , orientation : Orientation
+    }
+
+
+type Character
+    = CharacterA
+    | CharacterB
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Browser.Events.onResize (\w h -> WindowResized w h)
+
+
+
+--UPDATE
+
+
+type Msg
+    = WindowResized Int Int
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        WindowResized w h ->
+            ( { model | orientation = .orientation <| classifyDevice { width = w, height = h } }, Cmd.none )
+
+
+
+-- VIEW
+
+
+view model =
+    Element.layout [] <| text "hello world"
 
 
 
@@ -13,3 +84,4 @@ main =
 -- player's dialogue choices in a list of buttons
 -- player chooses a dialogue option:
 -- character image
+--
