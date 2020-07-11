5718bd1cdfcb1b588466533aa85c38ceee422716
Angus Long <0362654@schoolsnet.act.edu.au>
2020-07-11 10:54:27 +1000

    installed mdgriffith/elm-ui

--- src/Main.elm
+++ src/Main.elm
@@ -5,3 +5,11 @@ import Html exposing (text)
 
 main =
     text "hello world"
+
+
+
+-- character image (alt -> character's name)
+-- their dialogue in a dialogue box
+-- player's dialogue choices in a list of buttons
+-- player chooses a dialogue option:
+-- character image
